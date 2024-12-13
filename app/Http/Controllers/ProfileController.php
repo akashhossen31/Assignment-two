b635aab289aeb62214507c8523ae09ce7cb237ff
Akash Hossen <mdakashhosen31@gmail.com>
2024-12-13 19:12:35 +0600

    Complete assignment two

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+class ProfileController extends Controller
+{
+    public function index($id)
+    {
+        $name = "Donald Trump";
+        $age = "75";
+        $data = [
+            'id' => $id,
+            'name' => $name,
+            'age' => $age,
+        ];
+
+        $cookieName = 'access_token';
+        $cookieValue = '123-XYZ';
+        $cookieMinutes = time() + 60;
+        $cookiePath = '/';
+        $cookieDomain = $_SERVER['SERVER_NAME'];
+        $cookieSecure = false;
+        $cookieHttpOnly = true;
+        $cookie = cookie( $cookieName, $cookieValue, $cookieMinutes, $cookiePath, $cookieDomain, $cookieSecure, $cookieHttpOnly);
+        return response()
+            ->json($data, 200)
+            ->cookie($cookie);
+    }
+}
